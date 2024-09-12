757ff38f1523a5ff4ae67f7512b7ebe418c6a1ef
ag-nugroho <nugrohoagung757@gmail.com>
2024-09-12 10:30:07 +0700

    Praktikum 2

---
+++ Pertemuan-2/pertemuan_2/bin/pertemuan_2.dart
@@ -0,0 +1,7 @@
+import 'package:pertemuan_2/pertemuan_2.dart' as pertemuan_2;
+
+void main() {
+  for (int i = 20; i > 10; i--) {
+    print('Nama saya adalah Agung, sekarang berumur ${i - 2}');
+  }
+}
